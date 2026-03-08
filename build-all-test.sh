de58458476f8dac0ecab1b46703b5bc884570bae
A00336144 <A00336144@student.tus.ie>
2026-03-08 04:44:59 +0000

    Jenkins testing v3

--- build-all-test.sh
+++ build-all-test.sh
@@ -9,7 +9,7 @@ for service in room-service booking-service; do
   echo "Building $service..."
   cd "$ROOT_DIR/services/$service"
   chmod +x mvnw
-  ./mvnw clean test package
+  mvn clean test package
 done
 
 echo "All builds completed successfully"
